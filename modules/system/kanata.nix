577dbf5ab20341fab35afeacbe8cf58d2484d1ad
Kryštof Gärtner <krystofgartner@proton.me>
2026-06-07 21:02:19 +0200

    run kanata as systemd service

--- modules/system/kanata.nix
+++ modules/system/kanata.nix
@@ -1,15 +1,23 @@
-# Kanata keyboard remapper.
-{ pkgs, ... }:
+# Kanata keyboard remapper, run as a systemd service (kanata-msi.service).
+{ lib, pkgs, ... }:
 
 {
+    # The kanata CLI on PATH (for `kanata --check ...` etc.).
     environment.systemPackages = [ pkgs.kanata ];
 
-    # kanata writes to a virtual keyboard via /dev/uinput
-    hardware.uinput.enable = true;
-    boot.kernelModules = [ "uinput" ];
-    users.groups.uinput = { };
+    # The service brings its own kanata copy + enables kernel uinput, etc...
+    services.kanata = {
+        enable = true;
+        keyboards.msi = {
+            configFile = "/home/lmnk/repos/dotfiles/tilde/.config/kanata/kanata.kbd";
+        };
+    };
 
-    # merged into the user's extraGroups defined in default.nix
+    systemd.services.kanata-msi.serviceConfig = {
+        ProtectHome = lib.mkForce "tmpfs";
+        BindReadOnlyPaths = [ "/home/lmnk/repos/dotfiles/tilde/.config/kanata" ];
+    };
+
     users.users.lmnk.extraGroups = [
         "input"
         "uinput"
